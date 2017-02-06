7ddaa9491748c9134430dc211db4bc2304bc3d18
JoelBot <Joel.Lonneker@gmail.com>
2017-02-06 11:14:25 -0800

    some work done

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,10 +1,17 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) (
+    function EventController($scope) {
         $scope.event = {
             name: 'Angular Boot Camp',
-            tate: '1/1/2013',
-            time: '10:30 am'
-        };
-    ))
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png'
+        }
+    }
+);
